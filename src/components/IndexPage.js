17f08e7d33a9ca99c3871ae6042b9b7b25fb831c
Olexandr Balyk <olexandr.balyk@ucc.ie>
2022-04-12 02:04:00 +0200

    Display info only on tim-scenario repositories

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -9,17 +9,21 @@ function IndexPage(props) {
 
   return (
     repositories && (
-      <Row
-        xs={"auto"}
-        className="charts py-2 justify-content-center justify-content-md-start"
-      >
-        {repositories.map((repository, idx) => (
-          <Col className="p-2" key={idx}>
-            <Card>
-              <Card.Body>{repository.name}</Card.Body>
-            </Card>
-          </Col>
-        ))}
+      <Row xs={"auto"} className="charts py-2 justify-content-center">
+        {repositories.map(
+          (repository, idx) =>
+            repository.topics.includes("tim-scenario") && (
+              <Col className="p-2" key={idx}>
+                <Card>
+                  <Card.Header>{repository.name}</Card.Header>
+                  <Card.Body>{repository.description}</Card.Body>
+                  <Card.Footer>
+                    <a href={repository.html_url}>{repository.html_url}</a>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            )
+        )}
       </Row>
     )
   );
