fb1358143b768cba66439171f4884f0c450e1a6b
Olexandr Balyk <olexandr.balyk@ucc.ie>
2022-04-13 14:36:07 +0200

    Update IndexPage according to Energy Charts

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -3,27 +3,29 @@ import { Row, Col, Card } from "react-bootstrap";
 import useFetchData from "../hooks/useFetch";
 
 function IndexPage(props) {
-  const repositories = useFetchData(
-    `https://api.github.com/orgs/MaREI-EPMG/repos`
+  const [isLoading, repositories] = useFetchData(
+    `https://api.github.com/orgs/MaREI-EPMG/repos`,
+    props.cache
   );
 
   return (
     repositories && (
       <Row xs={"auto"} className="charts py-2 justify-content-center">
-        {repositories.map(
-          (repository, idx) =>
-            repository.topics.includes("tim-scenario") && (
-              <Col className="p-2" key={idx}>
-                <Card>
-                  <Card.Header>{repository.name}</Card.Header>
-                  <Card.Body>{repository.description}</Card.Body>
-                  <Card.Footer>
-                    <a href={repository.html_url}>{repository.html_url}</a>
-                  </Card.Footer>
-                </Card>
-              </Col>
-            )
-        )}
+        {!isLoading &&
+          repositories.map(
+            (repository, idx) =>
+              repository.topics.includes("tim-scenario") && (
+                <Col className="p-2" key={idx}>
+                  <Card>
+                    <Card.Header>{repository.name}</Card.Header>
+                    <Card.Body>{repository.description}</Card.Body>
+                    <Card.Footer>
+                      <a href={repository.html_url}>{repository.html_url}</a>
+                    </Card.Footer>
+                  </Card>
+                </Col>
+              )
+          )}
       </Row>
     )
   );
